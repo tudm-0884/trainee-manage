39264187edb89bb01529ee295c6f3b7c27064f38
tudm-0884 <tudm-0884>
2019-05-08 17:40:17 +0700

    fix repository

--- app/Repositories/Contracts/CourseRepository.php
+++ app/Repositories/Contracts/CourseRepository.php
@@ -3,7 +3,7 @@
 namespace App\Repositories\Contracts;
 
 use App\Repositories\CourseRepositoryInterface;
-use App\Repositories\BaseRepository;
+use App\Repositories\Contracts\BaseRepository;
 use App\Models\Course;
 
 class CourseRepository extends BaseRepository implements CourseRepositoryInterface
